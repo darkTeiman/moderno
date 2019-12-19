e0738cf5fa73f0d4675b1523a3bb336ca9474f9c
darkTeiman <Alex.Teiman@yandex.by>
2019-12-19 20:39:59 +0300

    added adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -12,8 +12,35 @@ $(function () {
     dots: true,
     arrows: false,
     slidesToShow: 4,
-    slidesToScroll: 4
-
+    slidesToScroll: 4,
+     responsive: [{
+           breakpoint: 1900,
+           settings: {
+             slidesToShow: 3,
+             slidesToScroll: 3,
+             infinite: true,
+             dots: true
+           }
+         },
+         {
+         breakpoint: 1401,
+           settings: {
+             slidesToShow: 2,
+             slidesToScroll: 1,
+             infinite: true,
+             dots: true
+           }
+         },
+         {
+           breakpoint: 800,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1,
+             infinite: true,
+             dots: true
+           }
+         },
+        ]
   });
 
   $(".js-range-slider").ionRangeSlider({
@@ -47,6 +74,7 @@ $(function () {
     return false;
   });
 
+    $('input[type="file"], select').styler();
 
   $('.menu__btn').on('click', function () {
     $('.menu__list').slideToggle();
